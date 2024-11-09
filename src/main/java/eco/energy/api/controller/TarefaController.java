80b11501c86c72ffc2d7ce55dda17cc7b7eae885
matteocortezi <developer.mateusv@gmail.com>
2024-11-09 15:35:45 -0300

    Criar estrutura básica dos controladores

---
+++ src/main/java/eco/energy/api/controller/TarefaController.java
@@ -0,0 +1,14 @@
+package eco.energy.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tarefa")
+public class TarefaController {
+    @PostMapping
+    public void cadastrar(){
+
+    }
+}
