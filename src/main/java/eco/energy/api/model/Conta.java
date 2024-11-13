4a43b5d192633ed5d2d0c6e2f6de085433ba6704
Mateus Vinicius da Conceição Silva <166639387+matteocortezi@users.noreply.github.com>
2024-11-12 21:05:16 -0300

    Adicionar o colunm name corretamente mapeado com o banco de dados

--- src/main/java/eco/energy/api/model/Conta.java
+++ src/main/java/eco/energy/api/model/Conta.java
@@ -17,18 +17,22 @@ public class Conta {
 
  @Id
  @GeneratedValue(strategy = GenerationType.IDENTITY)
+ @Column(name = "id_conta", nullable = false)
  private Long id;
 
  @NotNull(message = "O valor total não pode ser nulo")
  @DecimalMin(value = "0.01", message = "O valor total deve ser maior que 0")
+ @Column(name = "valor_conta", nullable = false)
  private Double valorTotal;
 
  @NotNull(message = "O consumo de kWh não pode ser nulo")
  @DecimalMin(value = "0.01", message = "O consumo de kWh deve ser maior que 0")
+ @Column(name = "consumo_kwt_conta", nullable = false)
  private Double consumoKwh;
 
  @NotNull(message = "O mês não pode ser nulo")
  @Enumerated(EnumType.STRING)
+ @Column(name = "mes_conta", nullable = false)
  private Mes mes;
 
  public Conta(DadosCadastroConta dados) {
--- src/main/java/eco/energy/api/model/Tarefa.java
+++ src/main/java/eco/energy/api/model/Tarefa.java
@@ -17,17 +17,21 @@ public class Tarefa {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tarefa", nullable = false)
     private Long id;
 
     @NotBlank(message = "O nome da tarefa não pode ser vazio")
     @Size(min = 3, max = 100, message = "O nome da tarefa deve ter entre 3 e 100 caracteres")
+    @Column(name = "nome_tarefa", nullable = false)
     private String nome;
 
     @NotBlank(message = "A descrição não pode ser vazia")
     @Size(min = 5, max = 255, message = "A descrição deve ter entre 5 e 255 caracteres")
+    @Column(name = "descricao_tarefa", nullable = false)
     private String descricao;
 
     @NotNull(message = "O status não pode ser nulo")
+    @Column(name = "status_tarefa", nullable = true)
     private Boolean status; // true para completada, false para não completada
 
     public Tarefa(DadosCadastroTarefa dados){
